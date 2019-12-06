05b77733b24e8267d7a6ba6d75a69c29988059b7
Kenowak2200 <kevin.nowak@otto.de>
2019-12-06 09:31:30 +0100

    first draft Receiver structer

---
+++ Receiver.py
@@ -0,0 +1,9 @@
+class Receiver():
+    def __init__(self):
+        self.signal = 0
+
+    def get_signal(self):
+        return self.signal;
+
+    def set_signal(self, signal):
+        self.signal = signal
