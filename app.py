05532b934d392583f8682d4e0e238e7183a710c6
Kenowak2200 <kevin.nowak@otto.de>
2019-12-09 18:49:51 +0100

    created api

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request
+
+from components.CentralUnit import CentralUnit
+
+app = Flask(__name__)
+foo = CentralUnit
+
+
+@app.route("/")
+def health_check():
+    return app.response_class(status=200)
+
+
+@app.route("/signal", methods=["Post"])
+def receive_signal():
+    data = request.json
+
+    # TODO Receiver
+    response = app.response_class(
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+
+@app.route("/sensor", methods=["Post"])
+def send_sensor_status():
+    data = request.json
+    # Todo sensor magic
+    response = app.response_class(
+        status=200,
+        mimetype='application/json'
+    )
+
+
+if __name__ == '__main__':
+    app.run()
--- components/Lamp.py
+++ components/Lamp.py
@@ -1,3 +1,15 @@
+from datetime import time
+
+
 class Lamp:
     def __init__(self):
-        self.ligth = True
+        self.ligth = False
+
+    def let_it_glow(self):
+        set.ligth = True
+        time.sleep(15)
+        set.ligth = False
+
+    def start_thread(self):
+        # x = threading.Thread(target=)
+        pass
