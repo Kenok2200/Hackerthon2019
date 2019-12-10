e5ae07ae0f68b832a6d0b938686c2ec6c2777484
Kenowak2200 <kevin.nowak@otto.de>
2019-12-10 11:59:06 +0100

    app changes

--- components/Lamp.py
+++ components/Lamp.py
@@ -1,6 +1,10 @@
+# import RPi.GPIO as GPIO
+# import time as time
+
 class Lamp:
     # TODO
     def ligth_on(self):
+        # GPIO.setmode()GPIO.BCM)
+        # GPIO.setup(23,GPIO.OUT)
+        # GPIO.output(23,GPIO.HIGH)
         pass
-
-    pass
