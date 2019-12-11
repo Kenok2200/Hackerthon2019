ed639de15e08b4ad9a31d602df258534a7a6d947
Kenowak2200 <kevin.nowak@otto.de>
2019-12-11 10:42:26 +0100

    added timer script

--- TimerScript.py
+++ TimerScript.py
@@ -1,6 +1,17 @@
+import time
+
+import requests
+
+
 def main():
     while True:
-        sleep(15)
+        time.sleep(5)
+        requests.post(
+            "http://127.0.0.1:8080/signal",
+            data=None,
+            json=None
+
+        )
 
 
 if __name__ == '__main__':
