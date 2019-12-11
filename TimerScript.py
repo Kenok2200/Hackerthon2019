42660652b29f13a921e5382ee8c59565c848b191
Kenowak2200 <kevin.nowak@otto.de>
2019-12-11 13:04:53 +0100

    added timer script

--- TimerScript.py
+++ TimerScript.py
@@ -7,7 +7,7 @@ def main():
     while True:
         time.sleep(5)
         requests.post(
-            "http://127.0.0.1:8080/signal",
+            "http://127.0.0.1:8080/timer",
             data=None,
             json=None
 
